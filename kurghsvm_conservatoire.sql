48ec2bf778ee831344da371c4fdc265361ab41c2
MICLEA Christian <66925033+MicleaChristian@users.noreply.github.com>
2023-05-24 00:39:21 +0200

    AAAAAAAAAH (juste la bdd qui beug je vais crever)

--- kurghsvm_conservatoire.sql
+++ kurghsvm_conservatoire.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 24, 2023 at 12:27 AM
+-- Generation Time: May 24, 2023 at 12:38 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -20,6 +20,149 @@ SET time_zone = "+00:00";
 --
 -- Database: `kurghsvm_conservatoire`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `eleve`
+--
+
+CREATE TABLE `eleve` (
+  `IDELEVE` int(11) NOT NULL,
+  `NIVEAU` int(11) NOT NULL,
+  `BOURSE` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `eleve`
+--
+
+INSERT INTO `eleve` (`IDELEVE`, `NIVEAU`, `BOURSE`) VALUES
+(42, 3, 1),
+(54, 3, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `heure`
+--
+
+CREATE TABLE `heure` (
+  `TRANCHE` char(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `heure`
+--
+
+INSERT INTO `heure` (`TRANCHE`) VALUES
+('10:00-11:00'),
+('11:00-12:00'),
+('12:00-13:00'),
+('13:00-14:00'),
+('14:00-15:00'),
+('15:00-16:00'),
+('16:00-17:00'),
+('17:00-18:00'),
+('8:00-9:00'),
+('9:00-10:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inscription`
+--
+
+CREATE TABLE `inscription` (
+  `IDPROF` int(11) NOT NULL,
+  `IDELEVE` int(11) NOT NULL,
+  `NUMSEANCE` int(11) NOT NULL,
+  `DATEINSCRIPTION` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `instrument`
+--
+
+CREATE TABLE `instrument` (
+  `LIBELLE` char(32) NOT NULL,
+  `id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `instrument`
+--
+
+INSERT INTO `instrument` (`LIBELLE`, `id`) VALUES
+('Basse', 1),
+('Batterie', 2),
+('Clarinette', 3),
+('Flûte traversière', 4),
+('Guitare', 5),
+('Piano', 6),
+('Saxophone', 7),
+('Trombone', 8),
+('Trompette', 9),
+('Violon', 10);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jour`
+--
+
+CREATE TABLE `jour` (
+  `JOUR` char(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `jour`
+--
+
+INSERT INTO `jour` (`JOUR`) VALUES
+('Dimanche'),
+('Jeudi'),
+('Lundi'),
+('Mardi'),
+('Mercredi'),
+('Samedi'),
+('Vendredi');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `niveau`
+--
+
+CREATE TABLE `niveau` (
+  `NIVEAU` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `niveau`
+--
+
+INSERT INTO `niveau` (`NIVEAU`) VALUES
+(1),
+(2),
+(3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payer`
+--
+
+CREATE TABLE `payer` (
+  `IDPROF` int(11) NOT NULL,
+  `IDELEVE` int(11) NOT NULL,
+  `NUMSEANCE` int(11) NOT NULL,
+  `LIBELLE` char(32) NOT NULL,
+  `DATEPAIEMENT` date DEFAULT NULL,
+  `PAYE` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -46,15 +189,162 @@ INSERT INTO `personne` (`ID`, `NOM`, `PRENOM`, `TEL`, `MAIL`, `ADRESSE`) VALUES
 (54, 'Miclea', 'Alexandre', 663433569, 'micleaalex101@icloud.com', 'Rue Albert Sorel'),
 (55, 'Miclea', 'Christian', 663433569, 'engichris32@gmail.com', 'Rue Albert Sorel');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prof`
+--
+
+CREATE TABLE `prof` (
+  `IDPROF` int(11) NOT NULL,
+  `INSTRUMENT` char(32) NOT NULL,
+  `SALAIRE` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `prof`
+--
+
+INSERT INTO `prof` (`IDPROF`, `INSTRUMENT`, `SALAIRE`) VALUES
+(52, 'Guitare', 50000),
+(55, 'Piano', 100000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `seance`
+--
+
+CREATE TABLE `seance` (
+  `IDPROF` int(11) NOT NULL,
+  `NUMSEANCE` int(11) NOT NULL,
+  `TRANCHE` char(32) NOT NULL,
+  `JOUR` char(32) NOT NULL,
+  `NIVEAU` int(11) NOT NULL,
+  `CAPACITE` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trim`
+--
+
+CREATE TABLE `trim` (
+  `LIBELLE` char(32) NOT NULL,
+  `DATEFIN` char(32) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `trim`
+--
+
+INSERT INTO `trim` (`LIBELLE`, `DATEFIN`) VALUES
+('Trimestre 1', '2023-07-30');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` text NOT NULL,
+  `password` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `password`) VALUES
+('test', 'pass'),
+('christian', 'azer');
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `eleve`
+--
+ALTER TABLE `eleve`
+  ADD PRIMARY KEY (`IDELEVE`),
+  ADD KEY `I_FK_ELEVE_NIVEAU` (`NIVEAU`);
+
+--
+-- Indexes for table `heure`
+--
+ALTER TABLE `heure`
+  ADD PRIMARY KEY (`TRANCHE`);
+
+--
+-- Indexes for table `inscription`
+--
+ALTER TABLE `inscription`
+  ADD PRIMARY KEY (`IDPROF`,`IDELEVE`,`NUMSEANCE`),
+  ADD KEY `I_FK_INSCRIPTION_ELEVE` (`IDELEVE`),
+  ADD KEY `I_FK_INSCRIPTION_SEANCE` (`IDPROF`,`NUMSEANCE`),
+  ADD KEY `fk_numSeance` (`NUMSEANCE`);
+
+--
+-- Indexes for table `instrument`
+--
+ALTER TABLE `instrument`
+  ADD PRIMARY KEY (`LIBELLE`);
+
+--
+-- Indexes for table `jour`
+--
+ALTER TABLE `jour`
+  ADD PRIMARY KEY (`JOUR`);
+
+--
+-- Indexes for table `niveau`
+--
+ALTER TABLE `niveau`
+  ADD PRIMARY KEY (`NIVEAU`),
+  ADD KEY `NIVEAU` (`NIVEAU`);
+
+--
+-- Indexes for table `payer`
+--
+ALTER TABLE `payer`
+  ADD PRIMARY KEY (`IDPROF`,`IDELEVE`,`NUMSEANCE`,`LIBELLE`),
+  ADD KEY `I_FK_PAYER_INSCRIPTION` (`IDPROF`,`IDELEVE`,`NUMSEANCE`),
+  ADD KEY `I_FK_PAYER_TRIM` (`LIBELLE`),
+  ADD KEY `fk_paye_eleve` (`IDELEVE`),
+  ADD KEY `fk_paye_numSeance` (`NUMSEANCE`);
 
 --
 -- Indexes for table `personne`
 --
 ALTER TABLE `personne`
   ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `prof`
+--
+ALTER TABLE `prof`
+  ADD PRIMARY KEY (`IDPROF`),
+  ADD KEY `I_FK_PROF_INSTRUMENT` (`INSTRUMENT`);
+
+--
+-- Indexes for table `seance`
+--
+ALTER TABLE `seance`
+  ADD PRIMARY KEY (`IDPROF`,`NUMSEANCE`),
+  ADD KEY `I_FK_SEANCE_JOUR` (`JOUR`),
+  ADD KEY `I_FK_SEANCE_NIVEAU` (`NIVEAU`),
+  ADD KEY `I_FK_SEANCE_PROF` (`IDPROF`),
+  ADD KEY `fk_tranche` (`TRANCHE`),
+  ADD KEY `NUMSEANCE` (`NUMSEANCE`);
+
+--
+-- Indexes for table `trim`
+--
+ALTER TABLE `trim`
+  ADD PRIMARY KEY (`LIBELLE`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -65,6 +355,50 @@ ALTER TABLE `personne`
 --
 ALTER TABLE `personne`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `eleve`
+--
+ALTER TABLE `eleve`
+  ADD CONSTRAINT `fk_idEleve` FOREIGN KEY (`IDELEVE`) REFERENCES `personne` (`ID`),
+  ADD CONSTRAINT `fk_niveau` FOREIGN KEY (`NIVEAU`) REFERENCES `niveau` (`NIVEAU`);
+
+--
+-- Constraints for table `inscription`
+--
+ALTER TABLE `inscription`
+  ADD CONSTRAINT `fk_insc_eleve` FOREIGN KEY (`IDELEVE`) REFERENCES `eleve` (`IDELEVE`),
+  ADD CONSTRAINT `fk_inscr_prof` FOREIGN KEY (`IDPROF`) REFERENCES `prof` (`IDPROF`),
+  ADD CONSTRAINT `fk_numSeance` FOREIGN KEY (`NUMSEANCE`) REFERENCES `seance` (`NUMSEANCE`);
+
+--
+-- Constraints for table `payer`
+--
+ALTER TABLE `payer`
+  ADD CONSTRAINT `fk_paye_eleve` FOREIGN KEY (`IDELEVE`) REFERENCES `eleve` (`IDELEVE`),
+  ADD CONSTRAINT `fk_paye_lib` FOREIGN KEY (`LIBELLE`) REFERENCES `trim` (`LIBELLE`),
+  ADD CONSTRAINT `fk_paye_numSeance` FOREIGN KEY (`NUMSEANCE`) REFERENCES `seance` (`NUMSEANCE`),
+  ADD CONSTRAINT `fk_paye_prof` FOREIGN KEY (`IDPROF`) REFERENCES `prof` (`IDPROF`);
+
+--
+-- Constraints for table `prof`
+--
+ALTER TABLE `prof`
+  ADD CONSTRAINT `fk_idProf` FOREIGN KEY (`IDPROF`) REFERENCES `personne` (`ID`),
+  ADD CONSTRAINT `fk_refInstrument` FOREIGN KEY (`INSTRUMENT`) REFERENCES `instrument` (`LIBELLE`);
+
+--
+-- Constraints for table `seance`
+--
+ALTER TABLE `seance`
+  ADD CONSTRAINT `fk_jour` FOREIGN KEY (`JOUR`) REFERENCES `jour` (`JOUR`),
+  ADD CONSTRAINT `fk_prof` FOREIGN KEY (`IDPROF`) REFERENCES `prof` (`IDPROF`),
+  ADD CONSTRAINT `fk_tranche` FOREIGN KEY (`TRANCHE`) REFERENCES `heure` (`TRANCHE`),
+  ADD CONSTRAINT `seance_ibfk_1` FOREIGN KEY (`NIVEAU`) REFERENCES `niveau` (`NIVEAU`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
