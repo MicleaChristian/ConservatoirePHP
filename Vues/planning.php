0c06c3fb5d66871706ec8b9566c32a2cd3b27d10
Mathis Boulais <https://github.com/Asukyy>
2023-06-01 05:22:47 +0200

    reglage footer

--- Vues/planning.php
+++ Vues/planning.php
@@ -78,33 +78,7 @@ MonPdo::checkSessionAndRedirect();
             </div>
         </div>
     </div>
-
-    <footer class="footer bg-light">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6">
-                    <h5>Informations de contact</h5>
-                    <ul class="list-unstyled">
-                        <li>Adresse : 123 rue des exemples, Ville, Pays</li>
-                        <li>Téléphone : +1234567890</li>
-                        <li>Email : info@example.com</li>
-                    </ul>
-                </div>
-                <div class="col-md-6">
-                    <h5>Liens utiles</h5>
-                    <ul class="list-unstyled">
-                        <li><a href="#">Accueil</a></li>
-                        <li><a href="#">À propos</a></li>
-                        <li><a href="#">Services</a></li>
-                        <li><a href="#">Contact</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="text-center py-3">
-            <p class="mb-0">&copy; 2023 Mon Entreprise. Tous droits réservés.</p>
-        </div>
-    </footer>
+    <?php include("footer/footer.php") ?>
 </body>
 
 </html>
--- footer/footer.php
+++ footer/footer.php
@@ -1,4 +1,26 @@
-<!-- fait moi un footer -->
-<footer class="footer bg-light">
-<p>&copy; <?php echo date("Y"); ?> Miclea Christian</p>
-</footer>
+<footer class="footer bg-light mt-auto">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h5>Informations de contact</h5>
+                    <ul class="list-unstyled">
+                        <li>Adresse : 123 rue des exemples, Ville, Pays</li>
+                        <li>Téléphone : +1234567890</li>
+                        <li>Email : info@example.com</li>
+                    </ul>
+                </div>
+                <div class="col-md-6">
+                    <h5>Liens utiles</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="#">Accueil</a></li>
+                        <li><a href="#">À propos</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="text-center py-3">
+            <p class="mb-0">&copy; 2023 Mon Entreprise. Tous droits réservés.</p>
+        </div>
+    </footer>
